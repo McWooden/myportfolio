933101a8abf35fd8c000def09508712f92b79077
McWooden <garryf7887@gmail.com>
2025-10-20 14:45:40 +0700

    fx trackng

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { InteractiveElement } from '@/app/components/InteractiveElement';
 export default function Home() {
   return (
     <section className="relative flex-1 flex flex-col items-center justify-center h-[100dvh] overflow-hidden my-bg-radial">
-      <InteractiveElement className="transform 0s ease-out" amountX={10} amountY={10}>
+      <InteractiveElement transition="transform .8s ease-out" amountX={8} amountY={8}>
         <div className="w-120 h-120 absolute -bottom-40 left-1/2 -translate-x-1/2 bg-radial from-transparent to-white/20 rounded-full"></div>
       </InteractiveElement>
 
