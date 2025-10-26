f2c78be605e9a35c9cbb1b8d1eaf3596a3f81a34
McWooden <garryf7887@gmail.com>
2025-10-26 11:07:37 +0700

    ketinggalan lumenu nya

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -72,7 +72,7 @@ export default function Navbar() {
         {/* Mobile menu */}
         <div id='mobile-navbar' className='flex md:hidden opacity-0'>
           <button onClick={() => setIsOpen(!isOpen)}>
-            <LuMenu size={30} />
+            <LuMenu size={30} fill='#300a44'/>
           </button>
         </div>
       </div>
