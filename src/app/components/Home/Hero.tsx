12e78720660e8fd264f07bd98593a5deb7f88f83
McWooden <garryf7887@gmail.com>
2025-10-29 08:02:21 +0700

    little update hero

--- src/app/components/Home/Hero.tsx
+++ src/app/components/Home/Hero.tsx
@@ -40,7 +40,7 @@ export default function Hero() {
       animate('#intro-paragraph-2', {
         opacity: [0, .8],
         duration: 3000,
-        delay: 2100,
+        delay: 3100,
         directions: 'alternate',
       });
 
@@ -72,7 +72,7 @@ export default function Hero() {
       style={{ paddingTop: `${navHeight}px` }}
     >
       <div className="text-white w-11/12 md:w-1/3 lg:w-6/12 mr-auto p-4 md:px-6 lg:px-8 h-fit xl:w-1/2 xl:mb-50">
-        <h2 id='intro-heading' className="text-2xl lg:text-4xl font-semibold opacity-0 my-1 md:my-4">Hi, <span id="name">Aku Huddin</span></h2>
+        <h2 id='intro-heading' className="text-2xl lg:text-4xl font-semibold opacity-0 my-1 md:my-4">Hi, <span id="name">aku Huddin</span>.</h2>
         <div className="flex flex-col text-md lg:text-xl xl:text-2xl leading-relaxed">
             <p id="intro-paragraph-1" className="opacity-0">
               Aku bantu orang bikin <span className="font-tertiary text-3xl">professional</span> web portofolio yang lagi <span className="font-tertiary text-3xl">trending</span>
